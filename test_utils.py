70bd74db147fa7c969672d46d96b6823a60b0634
Andrew Lane <andrew.lane@hobsons.com>
2020-12-30 13:44:22 -0500

    Fix test

--- test_utils.py
+++ test_utils.py
@@ -5,4 +5,4 @@ from benfords_law_utils.utils import hello_world
 
 
 def test_hello_world():
-    assert hello_world() == "Hello, benfords-law-utils"
+    assert hello_world() == "Hello, benfords-law-utils!"
