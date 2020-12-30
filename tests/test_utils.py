dd76d28243183dd7e6c53fd664312686ee7a3df4
Andrew Lane <andrew.lane@hobsons.com>
2020-12-30 18:39:56 -0500

    Implentation of ascii_art_bar_graph with the start of tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 import pytest
 
-from benfords_law_utils.utils import calculate_benford_stats
+from benfords_law_utils.utils import calculate_benford_stats, ascii_art_bar_graph
 
 
 def test_calculate_benford_stats_for_empty_input():
@@ -35,3 +35,38 @@ def test_calculate_benford_stats_for_simple_input():
     assert(total_count) == 5
     assert(ratios) == [.2, 0, .2, 0, 0, 0, .2, .4, 0]
     assert(counts) == [1, 0, 1, 0, 0, 0, 1, 2, 0]
+
+
+def test_ascii_art_bar_graph():
+    _, ratios, __ = calculate_benford_stats(
+        [111, 3000, 701234, 8675309, 8888]
+    )
+    bar_graph = ascii_art_bar_graph(ratios, 20)
+    assert(bar_graph) == """**********             Leading 1 Ratio: 20.000%
+               ^       Expected Ratio:  30.103%
+
+                       Leading 2 Ratio: 0.000%
+        ^              Expected Ratio:  17.609%
+
+**********             Leading 3 Ratio: 20.000%
+      ^                Expected Ratio:  12.494%
+
+                       Leading 4 Ratio: 0.000%
+    ^                  Expected Ratio:  9.691%
+
+                       Leading 5 Ratio: 0.000%
+   ^                   Expected Ratio:  7.918%
+
+                       Leading 6 Ratio: 0.000%
+   ^                   Expected Ratio:  6.695%
+
+**********             Leading 7 Ratio: 20.000%
+  ^                    Expected Ratio:  5.799%
+
+********************   Leading 8 Ratio: 40.000%
+  ^                    Expected Ratio:  5.115%
+
+                       Leading 9 Ratio: 0.000%
+  ^                    Expected Ratio:  4.576%
+
+"""
